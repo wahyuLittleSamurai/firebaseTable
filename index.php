6869d1b5d387068372399cc18fd36278f619a0b8
wahyuLittleSamurai <jamilwahyu53@gmail.com>
2019-06-12 12:30:18 +0700

    project wildan

---
+++ index.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html><html><head><script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://www.gstatic.com/firebasejs/4.9.0/firebase.js"></script>
+<script src="app.js"></script>
+<!------ Include the above in your HEAD tag ----------></head><body><table style="width:100%" id="ex-table">	<thead>		<tr>			<th>id</th>			<th>lat</th> 			<th>long</th>			<th>sebagai</th>			<th>title</th>		</tr>    </thead>	<tbody>			</tbody> </table> </body>
